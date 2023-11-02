2a7e6509bb0a39de0a7d0f5f10bd2917900c10db
rvenecia <ryangvenecia@gmail.com>
2023-11-02 11:56:25 -0700

    quick drop done

--- tetrisclone.cpp
+++ tetrisclone.cpp
@@ -149,6 +149,9 @@ void GameLoop() {
     case 'k':
       tetromino.Rotate();
       break;
+    case 'i':
+      tetromino.QuickDrop(board);
+      break;
     case 'q':
       delwin(window);
       delwin(highscore);
